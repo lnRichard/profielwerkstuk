076b4608b3d9218a1be23a06a932acf75b042325
Tim-V123 <91962928+Tim-V123@users.noreply.github.com>
2021-12-06 21:15:52 +0100

    kamer 4 en enemies toegevoegd

---
+++ scenes/characters/enemies/gezichtsverf_grunt/Gezichtsverf_grunt.gd
@@ -0,0 +1,21 @@
+extends "res://scenes/characters/enemies/hostile_entity/HostileEntity.gd"
+
+
+func _ready():
+	apply_sight()
+	apply_multiplier()
+
+func _init().(20, 1, 10, 10, 10, 10, 1):
+	pass
+func _physics_process(delta):
+	
+	match state:
+		IDLE:
+			$AnimatedSprite.play("idle")
+		CHASING:
+			var vel = global_position.direction_to(player.global_position)
+			$AnimatedSprite.flip_h = vel.x < 0;
+			$AnimatedSprite.play("running")
+			move_and_collide(vel)
+		ATTACKING:
+			pass
