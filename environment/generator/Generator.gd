904479e9081f17403906bb852e644f6664bc6e2b
Angus <superangus@protonmail.com>
2021-12-30 17:29:26 +0100

    switched to astar, nav2d doesn't work procedurally

--- environment/generator/Generator.gd
+++ environment/generator/Generator.gd
@@ -3,21 +3,25 @@ extends Node2D
 var exit_set = false;
 var entrance_set = false;
 
+
 onready var noise = OpenSimplexNoise.new();
 var dungeon_size = Vector2(100, 100);
 
 var tile_cap = 0.2;
 var entrance_exit_cap = -0.5;
 
-onready var autotile = get_node("Navigation2D/Map");
+onready var autotile = $Map
+
+onready var astar = AStar2D.new()
 
 
-onready var nav: Navigation2D = $Navigation2D;
-
+var et_p;
+var ex_p;
+var points = [];
 func _ready():
-#	randomize()
+	randomize()
 	noise.seed = randi();
-	noise.octaves = 2.0;
+	noise.octaves = 5.0;
 	noise.period = 5;
 	place_tiles()
 	valid_path();
@@ -27,27 +31,30 @@ func place_tiles():
 		for y in dungeon_size.y:
 			var c = noise.get_noise_2d(x, y);
 			if c < tile_cap:
+				var thispoint = (x*dungeon_size.x)+y;
+				astar.add_point(thispoint, Vector2(x, y))
 				autotile.set_cell(x, y, 0);
+				if astar.has_point(((x-1)*dungeon_size.x)+y):
+					astar.connect_points(thispoint, ((x-1)*dungeon_size.x)+y)
+				if astar.has_point((x*dungeon_size.x)+y-1):
+					astar.connect_points(thispoint, ((x)*dungeon_size.x)+y-1)
 			if c < entrance_exit_cap && !entrance_set:
 				$Entrance.position = Vector2((x+1)*16, (y+1)*16);
-				print(Vector2(x*16, y*16))
+				print($Entrance.position)
 				entrance_set = true;
+				et_p = (x*dungeon_size.x)+y;
 	for x in range(dungeon_size.x-1, -1, -1):
 		for y in range(dungeon_size.y-1, -1, -1):
-			
 			var c = noise.get_noise_2d(x, y);
 			if c < entrance_exit_cap && !exit_set:
-				print(Vector2(x*16, y*16))
-				$Exit.position = Vector2(x*16, y*16);
+				$Exit.position = Vector2(x*16, (y-1)*16);
+				print($Exit.position);
 				exit_set = true;
+				ex_p = (x*dungeon_size.x)+y;
 				break;
 		if exit_set:
 			break
 	autotile.update_bitmask_region(Vector2(0, 0), dungeon_size)
 
 func valid_path():
-	var path = nav.get_simple_path($Entrance.global_position, $Exit.global_position);
-#	var path = nav.get_simple_path(Vector2(10, 10), Vector2(20, 20));
-	print(path)
-#	print($Entrance.global_position, $Exit.global_position)
-#	$Line2D.points = path;
+	print(astar.get_id_path(et_p, ex_p))
