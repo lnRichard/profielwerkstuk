e4053e1fb81dffeb6e3d0c9ff007d4bb2464f002
Angus <superangus@protonmail.com>
2021-12-30 13:53:55 +0100

    nav mesh on tilemap

--- environment/generator/Generator.gd
+++ environment/generator/Generator.gd
@@ -9,19 +9,25 @@ var dungeon_size = Vector2(100, 100);
 var tile_cap = 0.2;
 var entrance_exit_cap = -0.5;
 
+onready var autotile = get_node("Navigation2D/Map");
+
+
+onready var nav: Navigation2D = $Navigation2D;
+
 func _ready():
-	randomize()
+#	randomize()
 	noise.seed = randi();
 	noise.octaves = 2.0;
 	noise.period = 5;
 	place_tiles()
+	valid_path();
 	
 func place_tiles():
 	for x in dungeon_size.x:
 		for y in dungeon_size.y:
 			var c = noise.get_noise_2d(x, y);
 			if c < tile_cap:
-				$PlainDungeonAutoTile.set_cell(x, y, 0);
+				autotile.set_cell(x, y, 0);
 			if c < entrance_exit_cap && !entrance_set:
 				$Entrance.position = Vector2((x+1)*16, (y+1)*16);
 				print(Vector2(x*16, y*16))
@@ -37,4 +43,11 @@ func place_tiles():
 				break;
 		if exit_set:
 			break
-	$PlainDungeonAutoTile.update_bitmask_region(Vector2(0, 0), dungeon_size)
+	autotile.update_bitmask_region(Vector2(0, 0), dungeon_size)
+
+func valid_path():
+	var path = nav.get_simple_path($Entrance.global_position, $Exit.global_position);
+#	var path = nav.get_simple_path(Vector2(10, 10), Vector2(20, 20));
+	print(path)
+#	print($Entrance.global_position, $Exit.global_position)
+#	$Line2D.points = path;
