5c7ab39049e923b3425c446f2e8ba488093dee7b
lnRichard <2004040203@sondervick.nl>
2022-01-31 17:17:13 +0100

    Add flash pickup

---
+++ props/Flask/Flask.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+class_name Flask
+
+# on_pickup()
+var health_amount = 20 # Amount to heal entity
+
+# Health entity on pikcup
+func on_pickup(body: CollisionObject2D):
+	body.set_health(body.current_health + health_amount)
+	queue_free()
+
+func _on_Pickup_body_entered(body: CollisionObject2D):
+	on_pickup(body)
